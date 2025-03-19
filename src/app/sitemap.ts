17ccd22ae2851a1a891f88add589899cb6cc0dcf
blucecao <595268594@qq.com>
2025-03-19 19:37:57 +0800

    fix

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -25,19 +25,19 @@ export default function sitemap(): MetadataRoute.Sitemap {
         priority: 0.8,
       },
       {
-        url: 'privacy-policy', // privacy-policy
+        url: 'article/privacy-policy', // privacy-policy
         lastModified: new Date("2025-03-15 13:13:00"),
         changeFrequency: 'daily',
         priority: 0.5,
       },
       {
-        url: 'terms-of-service', // terms-of-service
+        url: 'article/terms-of-service', // terms-of-service
         lastModified: new Date("2025-03-15 13:13:00"),
         changeFrequency: 'daily',
         priority: 0.5,
       },
       {
-        url: 'about', // about
+        url: 'article/about', // about
         lastModified: new Date("2025-03-15 13:13:00"),
         changeFrequency: 'daily',
         priority: 0.7,
